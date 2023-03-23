d5440a37959a13cb96ca06e8997c21cd4740c788
Lucas Anand Mazotti Ferretti <82159593+lucasanand9@users.noreply.github.com>
2023-03-23 16:08:43 -0300

    Add files via upload

--- maiorQue.hs
+++ maiorQue.hs
@@ -4,4 +4,6 @@ maior3 a b c = if a > b && a > c then a else if b > c && b > a then b else c;
 
 maior3' a b c = maior2(maior2 a b) c
 
-maior4 a b c d = maior3'(maior2 a b ) c d
+maior4 a b c d = maior2 (maior2 a b) (maior2 c d)
+
+maior4' a b c d = maior3'(maior2 a b ) c d
